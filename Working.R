a497cd14068b6c89d1ca89a3d0320d7d6b483081
Crt Ahlin <crt.ahlin@gmail.com>
2015-06-23 07:36:06 +0200

    Comment code

--- Working.R
+++ Working.R
@@ -6,12 +6,17 @@ head(AIRSData)
 # load the library
 library(cyclicCurvesAlfa)
 
-# cyclic cubuc splines
-cyclicCubicSplineTPB(AIRSData[sample(1:1699655, size=1500),], coef = TRUE)
-cyclicCubicSplineTPB(AIRSData[sample(1:1699655, size=1500),], coef = FALSE)
+# cyclic cubic splines
+CCS1 <- cyclicCubicSplineTPB(AIRSData[sample(1:1699655, size=1500),], coef = TRUE)
+str(CCS1) # "solution" matrix
+CCS2 <- cyclicCubicSplineTPB(AIRSData[sample(1:1699655, size=1500),], coef = FALSE)
+str(CCS2) # regression coeficients 
+# curve
 cyclicCubicSplineTPBCurve(AIRSData[sample(1:1699655, size=2000),])
+# curve with confidence intervals
 cyclicCubicSplineTPBCurve(AIRSData[sample(1:1699655, size=1000),], curve = TRUE)
 
 # cyclic fractional polynomials
-cyclicFractionalPolynomial(AIRSData)
+cyclicFractionalPolynomial(AIRSData) # regression coeficients
+# curve
 cyclicFractionalPolynomialCurve(AIRSData[sample(1:1699655, size=10000),], curve=TRUE, add=FALSE)
