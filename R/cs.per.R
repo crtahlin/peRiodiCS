86024aa3600a75e32d9e32bae8666052c242fd2a
Crt Ahlin <crt.ahlin@gmail.com>
2016-09-24 22:06:31 +0200

    add help for cs.per()

--- R/cs.per.R
+++ R/cs.per.R
@@ -1,21 +1,20 @@
-#implementation of the formulas of zhang for any support of x
-
+#' @title Periodic cubic splines design matrix
+#' 
+#' @description implementation of the formulas of zhang for any support of x
+#' 
+#' @param x numeric vector
+#' @param knots vector with knot locations
+#' @param nk number of knots, overridden if knots are provided
+#' @param mxax value of the end of the cycle
+#' @param mxax value of the beginning of the cycle
+#' 
 #' @export
 cs.per=function(x, knots=NULL, nk=5, xmax=max(x, na.rm=TRUE), xmin=min(x, na.rm=TRUE)){
-  
-  #x: numeric vector
-  #knots: vecotor with knot locations
-  #nk: number of knots, overridden if knots are provided
-  #mxax: value of the end of the cycle
-  #mxax: value of the beginning of the cycle
-  
-  
+
   #derive the knots as in the rms package, if not provided
   if(is.null(knots)) {
     knots=rcspline.eval(x, nk=nk, knots.only=TRUE)
   }
-  
-  
   
   nk=length(knots)  
   
@@ -26,23 +25,19 @@ cs.per=function(x, knots=NULL, nk=5, xmax=max(x, na.rm=TRUE), xmin=min(x, na.rm=
     my.aj=-1/(xmax-xmin)*(  
       (xmax^2+xmin^2+4*xmin*xmax)/2*(xmax-knots[j])-
         3*(xmax+xmin)/2*(xmax-knots[j])^2+(xmax-knots[j])^3)
-    
-    
-    
+  
     my.bj=3*(xmax+xmin)*(xmax-knots[j])/(2*(xmax-xmin))-3*(xmax-knots[j])^2/(2*(xmax-xmin))
     
     my.cj=-(xmax-knots[j])/(xmax-xmin)
     
-    
     rcs.out[,j]=my.aj*x+my.bj*(x^2)+my.cj*(x^3)+ifelse(x-knots[j]>0, (x-knots[j])^3, 0)
-    
-    
+
   }
   
   #saves the knots
   attr(rcs.out, "knots") <- knots
   
+  #return result
+  return(rcs.out)
   
-  
-  rcs.out
 }#end b.rcs.cubic.2c
