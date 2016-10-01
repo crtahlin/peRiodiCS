825bb92088fda80d30e5cc415860809e087c23d3
Crt Ahlin <crt.ahlin@gmail.com>
2016-10-01 15:00:51 +0200

    add default parameter values

--- R/sine_function.R
+++ R/sine_function.R
@@ -1,7 +1,7 @@
 #' @title Sine function used in the simulations (for simulating probability)
 #' 
 #' @export
-sine_function <- function(par1sin, par2sin, par3sin, par4sin, par5sin) {
+sine_function <- function(x, par1sin=1, par2sin=0.25, par3sin=0.25, par4sin=1, par5sin=0) {
   result <- (par1sin + sin(x * 2 * pi * par4sin + (par5sin * 2 * pi) )) * par2sin + par3sin 
   return(result)
 }
