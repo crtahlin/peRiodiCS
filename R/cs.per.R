74a478ba8702d929de6173fd3791246734b85308
Crt Ahlin <crt.ahlin@gmail.com>
2018-06-26 12:26:58 +0200

    backup while pretifying

--- R/cs.per.R
+++ R/cs.per.R
@@ -9,35 +9,40 @@
 #' @param mmin value of the beginning of the cycle
 #' 
 #' @export
-cs.per=function(x, knots=NULL, nk=5, xmax=max(x, na.rm=TRUE), xmin=min(x, na.rm=TRUE)){
+cs.per=function(x, knots = NULL,
+                nk = 5,
+                xmax = max(x, na.rm=TRUE),
+                xmin = min(x, na.rm=TRUE)){
 
-  #derive the knots as in the rms package, if not provided
-  if(is.null(knots)) {
-    knots=rcspline.eval(x, nk=nk, knots.only=TRUE)
+  # if they are not provided, derive the knots using function from the rms package
+  if( is.null(knots) ) {
+    knots <- rcspline.eval(x, nk = nk, knots.only = TRUE)
   }
   
-  nk=length(knots)  
+  nk <- length(knots) # set number of knots
   
-  rcs.out=matrix(NA, ncol=nk, nrow=length(x))   
+  rcs.out <- matrix(NA, ncol = nk, nrow = length(x)) # prepare an empty design matrix
   
-  for(j in 1:length(knots)){
+  # do a loop across all columns of the design matrix, calculating the columns
+  for( j in 1:length(knots) ){ # TODO: zakaj se referencira na lenght knots in ne na "nk"? 
     
     my.aj=-1/(xmax-xmin)*(  
       (xmax^2+xmin^2+4*xmin*xmax)/2*(xmax-knots[j])-
         3*(xmax+xmin)/2*(xmax-knots[j])^2+(xmax-knots[j])^3)
   
-    my.bj=3*(xmax+xmin)*(xmax-knots[j])/(2*(xmax-xmin))-3*(xmax-knots[j])^2/(2*(xmax-xmin))
+    my.bj <- ((3 * (xmax + xmin) * (xmax - knots[j])) / (2*(xmax - xmin))) -
+      ((3 * (xmax - knots[j])^2) / (2 * (xmax - xmin))) # b_j
     
-    my.cj=-(xmax-knots[j])/(xmax-xmin)
+    my.cj <- ( - (xmax - knots[j])/(xmax - xmin)) # c_j
     
     rcs.out[,j]=my.aj*x+my.bj*(x^2)+my.cj*(x^3)+ifelse(x-knots[j]>0, (x-knots[j])^3, 0)
 
   }
   
-  #saves the knots
+  # saves the knot locations as attributes
   attr(rcs.out, "knots") <- knots
   
-  #return result
+  # return result
   return(rcs.out)
   
 }#end b.rcs.cubic.2c
